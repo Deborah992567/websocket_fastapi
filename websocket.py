2ac36efb8aa997da8ffeb091480342194b104d44
uchechiiwuh <debbieofficial214@gmail.com>
2025-08-08 19:20:06 +0200

    added the websocket for username

--- websocket.py
+++ websocket.py
@@ -3,14 +3,19 @@ from fastapi import WebSocket
 # connects different users 
 class ConnectManager:
     def __init__(self):
-        self.active_connection = list[WebSocket] = []
+        self.active_connection :dict[str ,WebSocket] = []
         
-        async def Connect(self , websocket:WebSocket):
+        async def Connect(self ,username , websocket:WebSocket):
             await websocket.accept()
-            self.active_connection.append(websocket)
+            self.active_connection[username] =websocket
             
-        async def disconnect(self , websocket:WebSocket):
-            self.active_connection.remove(websocket)
+        async def disconnect(self , username ,websocket:WebSocket):
+            self.active_connection.pop(username ,None)
+            
+        async def  personal_info(self , message:str , username:str ):
+            websocket = self.active_connection.get(username)
+            if websocket:
+                await websocket.send_text(message)
             
         async def broadcast(self , message:str):
             for connection in self.active_connection:
